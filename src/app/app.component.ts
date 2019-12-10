c82672fb8ed4095c502a90113dd818004f21c69e
sam <evaoosyz@gmail.com>
2019-12-10 15:34:19 +0800

    Introduction to Components Done

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component(
+  {
+    selector:'pm-root',
+    template:`
+    <div><h1>{{pageTitle}}</h1>
+    <div>My First Component</div>
+    </div>
+    `
+  }
+)
+
+export class AppComponent{
+  pageTitle :string ='Acme Product Mangement';
+}
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'pm-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Angular: Getting Started';
+// }
