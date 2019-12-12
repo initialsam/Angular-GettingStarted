3ee5df75eaebfbda8a254a5f37c60af3ea3f83db
sam <evaoosyz@gmail.com>
2019-12-12 11:04:00 +0800

    Navigation and Routing Additional Techniques   Done

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   selector: 'pm-product-detail',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-
-  constructor() { }
+  pageTitle = 'Product Detail';
+  product: IProduct ;
+  constructor(private route: ActivatedRoute,private router: Router) {}
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `:${id}`
+    this.product = {
+      "productId": id,
+      "productName": "Video Game Controller",
+      "productCode": "GMG-0042",
+      "releaseDate": "October 15, 2018",
+      "description": "Standard two-button video game controller",
+      "price": 35.95,
+      "starRating": 4.6,
+      "imageUrl": "assets/images/xbox-controller.png"
+    }
   }
-
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
