db0e51e3ba9f7d4adb208e32730d5de1ff95c10d
sam <evaoosyz@gmail.com>
2020-01-05 17:00:49 +0800

    加入 搜尋產品名稱

--- APM/src/app/products/product-list/product-list.component.ts
+++ APM/src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
   selector: "app-product-list",
@@ -10,8 +11,20 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = "cart";
-  products: any[] = [
+  _listFilter: string = "";
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter
+      ? this.performFilter(this.listFilter)
+      : this.products;
+  }
+
+  filteredProducts: IProduct[] = [];
+
+  products: IProduct[] = [
     {
       productId: 1,
       productName: "Leaf Rake",
@@ -36,9 +49,19 @@ export class ProductListComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.filteredProducts = this.products;
+  }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter(
+      (product: IProduct) =>
+        product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
   }
 }
