c11c7b9eab2c412eab6c45ec8e071de052ebfda0
sam <evaoosyz@gmail.com>
2020-01-07 21:40:59 +0800

    加入 Service

--- APM/src/app/products/product-list/product-list.component.ts
+++ APM/src/app/products/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductsService } from "../products.service";
 
 @Component({
   selector: "app-product-list",
@@ -24,32 +25,12 @@ export class ProductListComponent implements OnInit {
 
   filteredProducts: IProduct[] = [];
 
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: "Leaf Rake",
-      productCode: "GDN-0011",
-      releaseDate: "March 19, 2019",
-      description: "Leaf rake with 48-inch wooden handle.",
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: "assets/images/leaf_rake.png"
-    },
-    {
-      productId: 2,
-      productName: "Garden Cart",
-      productCode: "GDN-0023",
-      releaseDate: "March 18, 2019",
-      description: "15 gallon capacity rolling garden cart",
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: "assets/images/garden_cart.png"
-    }
-  ];
+  products: IProduct[] = [];
 
-  constructor() {}
+  constructor(private productsService: ProductsService) {}
 
   ngOnInit() {
+    this.products = this.productsService.getProducts();
     this.filteredProducts = this.products;
   }
 
