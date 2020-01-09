c71ac63e69784b28d336f39b7ce3626faacf5a1a
sam <evaoosyz@gmail.com>
2020-01-09 10:55:05 +0800

    加入 連結到產品明細頁

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { IProduct } from './product-list/product';
 
 @Component({
@@ -7,10 +9,16 @@ import { IProduct } from './product-list/product';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  pageTitle = 'Product Detail';
   product: IProduct;
-  constructor() { }
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle = 'Product Detail' + id;
   }
-
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
